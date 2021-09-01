bded1b1ac8f295d1656c00db5aafda4e1b1c02bf
kevin-d-mcewan <MacKevinProgramming@gmail.com>
2021-09-01 14:01:21 -0700

    embedding expressions

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,21 +1,25 @@
 import React, { Component } from "react";
 
-// This can also be written as the next line with 'export default Counter;' line being moved
-// export default class Counter extends Component {}    either way works 
 class Counter extends Component {
     state = {
-        count: 1
+        count: 13
     };
     
     render() {
-        // Technically is 'React.createElement('div') here but dont need to type it here done automatically
         return (
-            <React.Fragment>
-                <span>{this.props.count}</span>
+          <div>
+                <span>{this.formatCount()}</span>
                 <button>Increment</button>
-            </React.Fragment>
+              
+    
+          </div>
                 
         );
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? "Zero" : count;
     }
 }
 
