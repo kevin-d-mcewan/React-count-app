0a6ca58de479e4b409aeebd1088ec37b2b1ea712
kevin-d-mcewan <MacKevinProgramming@gmail.com>
2021-09-01 14:23:53 -0700

    setting attributes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,16 +2,24 @@ import React, { Component } from "react";
 
 class Counter extends Component {
     state = {
-        count: 13
+        count: 0,
+        imageUrl: 'https://picsum.photos/200'
     };
+
+    styles = {
+        fontWeight: 10,
+        fontSize: 25,
+    }
     
     render() {
         return (
-          <div>
-                <span>{this.formatCount()}</span>
-                <button>Increment</button>
-              
-    
+            <div>
+                
+                <span style={this.styles}className="d-flex badge badge-primary m-2">{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
+                <br></br>
+                <br></br>
+                <img src={this.state.imageUrl} alt="" />;
           </div>
                 
         );
