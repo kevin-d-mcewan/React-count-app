419bb7b3bd78e90e91ae40377e3e63045fffcf9e
kevin-d-mcewan <MacKevinProgramming@gmail.com>
2021-09-02 11:25:04 -0700

    HandlingEvents, RenderingEvents, ConditionalRendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,26 +3,51 @@ import React, { Component } from "react";
 class Counter extends Component {
     state = {
         count: 0,
-        imageUrl: 'https://picsum.photos/200'
+        imageUrl: 'https://picsum.photos/200',
+        tags: ['tag1', 'tag2', 'tag3'],
     };
 
     styles = {
         fontWeight: 10,
         fontSize: 25,
+    };
+
+    renderTags() {
+        if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+    }
+
+    handleIncrement() {
+        console.log('Increment Clicked');
     }
     
     render() {
         return (
             <div>
-                
-                <span style={this.styles}className="d-flex badge badge-primary m-2">{this.formatCount()}</span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
-                <br></br>
+                {/* <span style={this.styles}className="d-flex badge badge-primary m-2">{this.formatCount()}</span> */}
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
                 <br></br>
                 <img src={this.state.imageUrl} alt="" />;
+                <br></br>
+                <ul>
+                    {this.state.tags.map(tag => <li key={tag}>{ tag }</li>)}
+                </ul>
+                <div>
+                    {/* Right below would be 2nd option if didnt want to do 'renderTags() method */}
+                    {/* { this.state.tags.length === 0 && 'Please create a new tag!'} */}
+                    {this.renderTags()}
+                </div>
           </div>
                 
         );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
     }
 
     formatCount() {
