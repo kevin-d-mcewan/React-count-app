7999404693eef1aad523d86da7e5d6db923ba436
kevin-d-mcewan <MacKevinProgramming@gmail.com>
2021-09-16 16:25:27 -0700

    Used passing thru a children component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
     state = {
-        count: 0,
+        value: this.props.value,
     };
 
     styles = {
@@ -13,7 +13,7 @@ class Counter extends Component {
     handleIncrement = (product) => {
         //product is just the 'event' from DOM Events where we normally pass an 'e' which we could have done.
         console.log(product);
-        this.setState({ count: this.state.count + 1})
+        this.setState({ value: this.state.value + 1})
 
     }
 
@@ -26,8 +26,11 @@ class Counter extends Component {
     
     
     render() {
+        console.log(this.props);
+
         return (
             <div>
+                {this.props.children}
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button
                     onClick={() => this.handleIncrement(this.product)}
@@ -40,12 +43,12 @@ class Counter extends Component {
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
+        classes += this.state.value === 0 ? "warning" : "primary";
         return classes;
     }
 
     formatCount() {
-        const { count } = this.state;
+        const { value: count } = this.state;
         return count === 0 ? "Zero" : count;
     }
 }
