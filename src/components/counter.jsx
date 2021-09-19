9b2bf325c059ad96a13c0ceb43b4a269877da45c
kevin-d-mcewan <MacKevinProgramming@gmail.com>
2021-09-18 21:08:48 -0700

    Added a delete button to the counters component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -36,6 +36,7 @@ class Counter extends Component {
                     onClick={() => this.handleIncrement(this.product)}
                     className="btn btn-secondary btn-sm">
                     Increment</button>
+             <button onClick={this.props.onDelete} className="btn btn-danger btn-sm m-2">Delete</button>   
           </div>
                 
         );
